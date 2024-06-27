dd038c7cea4cfabc5c9e3bd4917580c3dee0ca78
Carol <carol@yeselearning.com.au>
2024-06-27 10:17:54 +1000

    update html to remoe engament and alignment

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,45 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const clients = ['ecohaven', 'profG', 'sunny', 'flora', 'whims', 'blossom', 'wellness', 'paed', 'petunia', 'comms',]; // Add all client IDs here
-  
-    clients.forEach(client => {
-      const criteria = ['budget', 'engagement', 'history', 'growth', 'alignment'];
-      
-      criteria.forEach(criterion => {
-        const selectElement = document.getElementById(`${criterion}-${client}`);
-        selectElement.addEventListener('change', () => calculateTotalScore(client));
-      });
-    });
-  
-    function calculateTotalScore(client) {
-      let totalScore = 0;
-      const criteriaWeights = {
-        'budget': 0.3,
-        'engagement': 0.3,
-        'history': 0.2,
-        'growth': 0.1,
-        'alignment': 0.1
-      };
-  
-      Object.keys(criteriaWeights).forEach(criterion => {
-        const selectElement = document.getElementById(`${criterion}-${client}`);
-        const value = parseFloat(selectElement.value);
-        totalScore += value * criteriaWeights[criterion];
-      });
-  
-      document.getElementById(`total-score-${client}`).textContent = totalScore.toFixed(1);
-    }
+function showTab(tabId) {
+  var tabs = document.querySelectorAll('.nav-tabs button');
+  tabs.forEach(function(tab) {
+      tab.classList.remove('active');
   });
+
+  var contents = document.querySelectorAll('.tab-content');
+  contents.forEach(function(content) {
+      content.style.display = 'none';
+  });
+
+  document.getElementById(tabId).style.display = 'block';
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  const clients = ['ecohaven', 'profG', 'sunny', 'flora', 'whims', 'blossom', 'wellness', 'paed', 'petunia', 'comms']; // Add all client IDs here
+
+  clients.forEach(client => {
+    const criteria = ['budget', 'history', 'growth'];
+    
+    criteria.forEach(criterion => {
+      const selectElement = document.getElementById(`${criterion}-${client}`);
+      selectElement.addEventListener('change', () => calculateTotalScore(client));
+    });
+  });
+
+  function calculateTotalScore(client) {
+    let totalScore = 0;
+    const criteriaWeights = {
+      'budget': 0.4,
+      'history': 0.3,
+      'growth': 0.3
+    };
+
+    Object.keys(criteriaWeights).forEach(criterion => {
+      const selectElement = document.getElementById(`${criterion}-${client}`);
+      const value = parseFloat(selectElement.value);
+      totalScore += value * criteriaWeights[criterion];
+    });
+
+    document.getElementById(`total-score-${client}`).textContent = totalScore.toFixed(1);
+  }
+});
+
   
